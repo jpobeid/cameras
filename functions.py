2bf07d10f2f57a6eead02bbec6a2a93371c5fb15
JP Obeid <jpobeid91@gmail.com>
2022-08-29 21:23:37 -0400

    Fixed n_cams function to detect with other videos mounted and improved main to close with gc and to be RPi specific

--- functions.py
+++ functions.py
@@ -5,10 +5,21 @@ COMMAND_LIST_CAMERAS = 'v4l2-ctl --list-devices'
 
 def get_n_cams():
     cams_process = subprocess.run(COMMAND_LIST_CAMERAS.split(' '), capture_output=True, text=True)
-    are_cameras_present = cams_process.stdout != ''
+    stdout_list = cams_process.stdout.split('\n')
+    # Filter stdout for relevate cams, not BCM ones
+    output_list = []
+    for i in range(len(stdout_list)):
+        line = stdout_list[i]
+        if line.startswith('bcm'):
+            to_ignore = True
+        elif line == '':
+            to_ignore = False
+        elif not to_ignore:
+            output_list.append(line)
+    are_cameras_present = len(output_list) != 0
     if are_cameras_present:
-        mounted_videos = re.findall(r'video\d', cams_process.stdout)
-        n_cams = len(mounted_videos) // 2
+        n_cams = sum(list(map(lambda x: not x.startswith('\t'), output_list)))
     else:
         n_cams = 0
     return n_cams
+    
--- main.py
+++ main.py
@@ -1,30 +1,35 @@
-import imp
-from multiprocessing.spawn import import_main_path
 import tkinter as tk
 import numpy as np
 from PIL import ImageTk, Image
 import cv2
 
+import gamecube # This will check for controller and stall
 import functions
-from gamecube import gc_endpoint, interpret
-import gamecube
 
-# First check for cams present
+ON_RPI = True
+
+# Setup GPIO
+if ON_RPI:
+    import RPi.GPIO as GPIO
+
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(37, GPIO.OUT)
+
+# Next check for cams present
 n_cams = functions.get_n_cams()
 if n_cams == 0:
     raise ValueError('No cameras detected by system...')
 else:
     cams = [cv2.VideoCapture(i) for i in range(0, 2 * n_cams, 2)]
-    print(f'Detected {n_cams} camera(s)...')
-    print(cams)
-
-# Second check for controller present, and stall till plugged
-# ENDPOINT = usb_reader.get_controller_endpoint()
+    print(f'Detected {n_cams} camera(s)...', cams)
 
 # Tk window
 INPUT_LOOP_DELAY = 50
+IMAGE_WIDTH = 200
+IMAGE_HEIGHT = 200
 
 _active_cam = 0
+_can_cam_switch = True
 
 win = tk.Tk()
 win.title('Mini app')
@@ -33,17 +38,30 @@ win.geometry('600x600')
 x, y = 0, 0
 
 def controller_input_loop():
-    global x, y, _active_cam
+    global x, y, _active_cam, _can_cam_switch
 
-    controller_bytes = gc_endpoint.read(8)[:-1]
+    controller_bytes = gamecube.gc_endpoint.read(8)[:-1]
     controller_output = gamecube.interpret(controller_bytes)
     signal_A = 'A' in controller_output.get('Main')
+    signal_B = 'B' in controller_output.get('Main')
     signal_ZR = 'ZR' in controller_output.get('Main')
     signal_ZL = 'ZL' in controller_output.get('Main')
+    signal_close = '-' in controller_output.get('Center')
 
-    if signal_ZL:
+    if ON_RPI:
+        if signal_B and GPIO.input(37) == 0:
+            GPIO.output(37, True)
+            print('37 is on')
+        elif not signal_B and GPIO.input(37) == 1:
+            GPIO.output(37, False)
+            print('37 is off')
+
+    if signal_ZL and _can_cam_switch:
         _active_cam = (_active_cam + 1) % n_cams
         print(f'Switched to camera {_active_cam}')
+        _can_cam_switch = False
+    elif not signal_ZL:
+        _can_cam_switch = True
 
     if signal_ZR:
         x, y = 0, 0
@@ -60,17 +78,23 @@ def controller_input_loop():
         print(ok)
         # BGR to RGB
         m_img = m_img[:, :, ::-1]
-        img = Image.fromarray(m_img).resize([300, 300])
+        img = Image.fromarray(m_img).resize([IMAGE_HEIGHT, IMAGE_WIDTH])
         img_tk = ImageTk.PhotoImage(img)
         panel.configure(image=img_tk)
         panel.image = img_tk
+
+    if signal_close:
+        print('Closing...')
+        if ON_RPI:
+            GPIO.cleanup()
+        win.destroy()
     
     win.after(INPUT_LOOP_DELAY, controller_input_loop)
 
 win.after(INPUT_LOOP_DELAY, controller_input_loop)
 
-img_tk = ImageTk.PhotoImage(Image.fromarray((np.random.rand(300, 300) * 256).astype(np.uint8)))
+img_tk = ImageTk.PhotoImage(Image.fromarray((np.zeros([IMAGE_HEIGHT, IMAGE_WIDTH]) * 256).astype(np.uint8)))
 panel = tk.Label(win, image=img_tk)
-panel.place(relx=x, rely=y, height=300, width=300)
+panel.place(relx=x, rely=y, height=IMAGE_HEIGHT, width=IMAGE_WIDTH)
 
 win.mainloop()
